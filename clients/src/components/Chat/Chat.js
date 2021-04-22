54f8ce539ee15e5ef79f2830bbf314cc5ebce15a
Rendy Artha <rendyartha312@gmail.com>
2021-04-22 22:56:06 +0700

    setup socket io clients chats

--- clients/src/components/Chat/Chat.js
+++ clients/src/components/Chat/Chat.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
+import queryString from 'query-string'
+import io from 'socket.io-client'
+import './Chat.css'
+
+let socket;
 
 const Chat = () => {
+  const location = useLocation()
+  const ENDPOINT = 'localhost:5000'
+
+  const [name, setName] = useState('')
+  const [room, setRoom] = useState('')
+  
+  useEffect(() => {
+    const {name, room} = queryString.parse(location.search)
+
+    socket = io(ENDPOINT)
+
+    setName(name)
+    setRoom(room)
+
+    socket.emit('join', { name, room }, () => {
+
+    })
+    return () => {
+      socket.emit('disconnect')
+      socket.off()
+    }
+  }, [ENDPOINT, location.search ])
+
   return (
     <div>
-      <h1>Join</h1>
+      <h1>Chat</h1>
     </div>
   )
 }
