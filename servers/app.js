ef2980adfafdfb7b4b851454b605b44820fa5dab
Rendy Artha <rendyartha312@gmail.com>
2021-04-22 22:55:37 +0700

    setup server socket

--- servers/app.js
+++ servers/app.js
@@ -2,17 +2,30 @@ const express = require('express')
 const socketio = require('socket.io')
 const http = require('http')
 const routes = require('./router')
+const cors = require('cors')
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
 const server = http.createServer(app)
-const io = socketio(server)
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    method: ["GET", "POST"],
+  },
+});
 
 app.use(routes)
+app.use(cors())
 
 io.on('connection', (socket) => {
   console.log('Connected')
+
+  socket.on('join', ({ name, room }, callback) => {
+    console.log(name, room)
+
+    // callback();
+  })
 
   socket.on('disconnect', () => {
     console.log('User Left')
