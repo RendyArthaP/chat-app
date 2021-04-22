691277180272fe88875b0272d8ad310a50b7e450
Rendy Artha <rendyartha312@gmail.com>
2021-04-22 20:54:42 +0700

    setup component

--- clients/src/App.js
+++ clients/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import { BrowserRouter as Router, Route} from 'react-router-dom'
+import Join from './components/Join/Join';
+import Chat from './components/Chat/Chat';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Route exact path="/">
+        <Join />
+      </Route>
+      <Route path="/chat">
+        <Chat />
+      </Route>
+    </Router>
   );
 }
 
