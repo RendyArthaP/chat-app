a6490fe5b64400976fe72cb31c89fa68e9411bee
Rendy Artha <rendyartha312@gmail.com>
2021-05-06 12:15:44 +0700

    setup server socket

--- servers/app.js
+++ servers/app.js
@@ -4,6 +4,13 @@ const http = require('http')
 const routes = require('./router')
 const cors = require('cors')
 
+const {
+  addUser,
+  removeUser,
+  getUser,
+  getUsersInRoom
+} = require('./users')
+
 const PORT = process.env.PORT || 5000
 
 const app = express()
@@ -19,13 +26,38 @@ app.use(routes)
 app.use(cors())
 
 io.on('connection', (socket) => {
-  console.log('Connected')
-
   socket.on('join', ({ name, room }, callback) => {
-    console.log(name, room)
+    const { error, user } = addUser({
+      id: socket.id,
+      name,
+      room
+    })
 
-    // callback();
+    if(error) return callback(error);
+
+    socket.emit('message', {
+      user: 'admin',
+      text: `Hello ${user.name} welcome to the ${user.room} room`
+    })
+    socket.broadcast.to(user.room).emit('message', {
+      user: 'admin',
+      text: `${user.name} has joined`
+    })
+    socket.join(user.room);
+
+    callback();
   })
+
+  socket.on('sendMessage', (message, callback) => {
+    const user = getUser(socket.id)
+
+    io.to(user.room).emit('message', {
+      user: user.name,
+      text: message
+    })
+
+    callback()
+  });
 
   socket.on('disconnect', () => {
     console.log('User Left')
